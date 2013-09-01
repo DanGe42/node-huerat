aa388d38a6e4b288ad2e497e777eb6658ef6edfb
Daniel Ge <dange@seas.upenn.edu>
2013-09-01 01:37:33 -0400

    Add routes for Hue

--- router.js
+++ router.js
@@ -3,7 +3,8 @@ var passport = require('passport');
 var routes = {
     index: require('./routes/index').index,
     settings: require('./routes/settings'),
-    auth: require('./routes/auth')
+    auth: require('./routes/auth'),
+    hue: require('./routes/hue')
 };
 
 var ensureAuthenticated = function(req, res, next) {
@@ -37,4 +38,10 @@ exports.route = function(app, sequelize) {
     });
     app.get('/login', routes.auth.login_page);
     app.get('/logout', routes.auth.logout);
+
+
+    /* Hue API */
+    app.get('/hue/bridges', ensureAuthenticated, routes.hue.findBridges);
+    app.post('/hue/bridge/:hostname/connect', ensureAuthenticated,
+        routes.hue.connectBridge.bind(null, models));
 };
---
+++ routes/hue.js
@@ -0,0 +1,85 @@
+var hue = require('node-hue-api');
+var crypto = require('crypto');
+
+/*
+ * GET /hue/bridges
+ *
+ * Returns a JSON-formatted list of bridges.
+ */
+exports.findBridges = function(req, res) {
+    hue.locateBridges()
+        .then(function(bridges) {
+            var json = {
+                message: 'Success!',
+                bridges: bridges
+            };
+            res.json(json);
+        })
+        .catch(function (err) {
+            console.error("Error occurred: " + err);
+            res.json(500, { message: 'Internal server error' });
+        })
+        .done();
+};
+
+/*
+ * POST /hue/bridge/:hostname/connect
+ *
+ * Connects to a bridge with the specified :hostname.
+ */
+exports.connectBridge = function(models, req, res) {
+    var user = req.user;
+    var hue_user = user.hue_user;
+    var hostname = req.params.hostname;
+    var hue_api = new hue.HueApi();
+
+    hue_api.registerUser(hostname, hue_user)
+        .then(function(hue_res) {
+            if (hue_res === hue_user) {
+                var Bridge = models.Bridge;
+                var handleErrors = function(err) {
+                    console.error(err);
+                    res.json(500, { message: 'Internal server error '});
+                };
+
+                Bridge.find({ where: { user_id: user.id } })
+                    .then(function(bridge) {
+                        if (bridge) {
+                            console.info('Removing old bridge.');
+                            return bridge.destroy();
+                        }
+                        return true;
+                    }, handleErrors)
+                    .then(function() {
+                        return Bridge.create({
+                            hostname: hostname,
+                            user_id: user.id
+                        });
+                    }, handleErrors)
+                    .then(function(bridge) {
+                        res.json({
+                            message: 'Success!',
+                            bridge: hostname,
+                            user: user.hue_user
+                        });
+                    }, handleErrors);
+            } else {
+                res.json(200, {
+                    code: 2,
+                    message: 'Request rejected by bridge.'
+                });
+            }
+        })
+        .fail(function(err) {
+            if (err.name === 'Api Error') {
+                res.json(200, {
+                    code: 1,
+                    message: 'Press link button on bridge and try again.'
+                });
+            } else {
+                console.error(err);
+                res.json(500, { message: 'Internal server error' });
+            }
+        })
+        .done();
+};
